aef2818f8ed6febfb350ef53dc886af909d375c8
Upme <lior.php@gmail.com>
2023-06-29 14:31:09 +0300

    Ver 0.0.1

--- src/AeolusCMS/Libs/AeolusObj.php
+++ src/AeolusCMS/Libs/AeolusObj.php
@@ -9,9 +9,15 @@ class AeolusObj {
     protected $post = null;
     /* @var dataObj $get */
     protected $get = null;
+    /* @var dataObj $request */
+    protected $request = null;
+    /* @var dataObj $env */
+    protected $env = null;
 
     public function __construct() {
         $this->post = App::$post;
         $this->get = App::$get;
+        $this->request = App::$request;
+        $this->env = App::$env;
     }
 }
